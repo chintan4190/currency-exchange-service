d14406e5904d53f72c07e6a527bb03a55da5422d
chintandoshi <chintan.doshi@rabobank.nl>
2019-06-02 16:19:20 +0200

    Initial Commit

---
+++ src/main/java/edu/springcloud/currency/exchange/controller/ExchangeValueRepository.java
@@ -0,0 +1,8 @@
+package edu.springcloud.currency.exchange.controller;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends 
+		JpaRepository<ExchangeValue, Long>{
+	ExchangeValue findByFromAndTo(String from, String to);
+}
