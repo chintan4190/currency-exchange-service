5988d17e09c168940276b8bc7a27c1db71c7835d
chintandoshi <chintan.doshi@rabobank.nl>
2019-06-08 18:26:32 +0200

    added zipkin and tracing

--- src/main/java/edu/springcloud/currency/exchange/CurrencyExchangeServiceApplication.java
+++ src/main/java/edu/springcloud/currency/exchange/CurrencyExchangeServiceApplication.java
@@ -1,8 +1,10 @@
 package edu.springcloud.currency.exchange;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -11,6 +13,11 @@ public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 
